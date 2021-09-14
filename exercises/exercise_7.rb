eee10af8252d403dd89d1079cc45cc8608f52052
gurpreet singh <gurpreet212004@gmail.com>
2021-09-14 01:02:12 +0000

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+puts "Enter store name:"
+@newstorename = gets.chomp
+
+
+# puts "Enter store revenue:"
+# @newstorevenue = gets.chomp.to_i
+
+# puts "Does store have mens apparel? true or false:"
+# @newstoremen = gets.chomp
+
+# puts "Does store have womens apparel? true or false:"
+# @newstorewomen = gets.chomp
+
+
+# @newStore = Store.create(name: "#{@newstorename}", annual_revenue: "#{@newstorevenue}", mens_apparel: "#{@newstoremen}", womens_apparel: "#{@newstorewomen}" )
+@newStore = Store.create(name: "#{@newstorename}")
+
+@newStore.errors.full_messages.each do |message|
+
+  puts message
+end
