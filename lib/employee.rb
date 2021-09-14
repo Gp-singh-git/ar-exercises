4c230ee99d219738cd25580fa443d30e6366033a
gurpreet singh <gurpreet212004@gmail.com>
2021-09-14 01:16:53 +0000

    exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,7 +2,7 @@ class Employee < ActiveRecord::Base
   belongs_to :store
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :hourly_rate, presence: true, inclusion:{in: 40..200}
+  validates :hourly_rate, presence: true, numericality: { only_integer: true}, inclusion:{in: 40..200}
   validates :store_id, presence: true
 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3}
-  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :mens_or_womens_apparel_must
 end
 
